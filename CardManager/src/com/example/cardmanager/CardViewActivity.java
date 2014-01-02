d539fb6c65b73cea6859b36daf54ca41dd0f21a5
Daniela Borges <alunassertiva@gmail.com>
2014-01-02 18:57:05 +0000

    styles no .java

--- CardManager/src/com/example/cardmanager/CardViewActivity.java
+++ CardManager/src/com/example/cardmanager/CardViewActivity.java
@@ -9,6 +9,8 @@ import android.content.Context;
 import android.content.Intent;
 import android.graphics.Typeface;
 import android.os.Bundle;
+import android.support.v4.app.NavUtils;
+import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.AdapterView;
@@ -24,10 +26,13 @@ public class CardViewActivity extends Activity {
 	    super.onCreate(savedInstanceState);
 	    setContentView(R.layout.activity_cardlistview);
 	    
+	    //to allow Up navigation with the app icon in the action bar
+	    getActionBar().setDisplayHomeAsUpEnabled(true);
+	    
 	    final ListView listview = (ListView) findViewById(R.id.cardlistview);
 	    
 	    //TODO ir buscar a base de dados, bem como as imagens
-	    String[] values = new String[] { "H3", "Chimarrão", "Resturante da Sé" };
+	    String[] values = new String[] { "H3", "Chimarrão", "Restaurante da Sé" };
 
 	    final ArrayList<String> list = new ArrayList<String>();
 	    for (int i = 0; i < values.length; ++i) {
@@ -38,14 +43,20 @@ public class CardViewActivity extends Activity {
 	    final Typeface fontsStyle = CustomFontsLoader.getTypeface(this.getBaseContext(), 
     			CustomFontsLoader.FONT_NAME_4_ELLIANAPATH);
 	 		    final StableArrayAdapter adapter = new StableArrayAdapter(this,
-	        android.R.layout.simple_list_item_1, list){	    
-	    /*	@Override
-	        public View getView(int position, View convertView, ViewGroup parent) {
-	            ViewGroup view = (ViewGroup) super.getView(position, convertView, parent);
-	            if(convertView == null) CardViewActivity.setAppFont(view, fontsStyle);
-	            return view;
-	        }*/
+	        android.R.layout.simple_list_item_1, list){
+	 		    	
+	 		@Override  
+	 		public View getView(int position, View view, ViewGroup viewGroup)
+	 		  {
+	 		  View v = super.getView(position, view, viewGroup);
+	 		  
+	 		  Typeface fontsStyle = CustomFontsLoader.getTypeface(getBaseContext(), CustomFontsLoader.FONT_NAME_3_CUPCAKES);
+	 		  ((TextView)v).setTextAppearance(getBaseContext(), R.style.UiCardTitleTextView);
+	 		  ((TextView)v).setTypeface(fontsStyle, Typeface.BOLD);
+	 		  return v;
+	 		  }
 	    };
+	    
 	    listview.setAdapter(adapter);
 	   
 	    listview.setOnItemClickListener(new AdapterView.OnItemClickListener() {
@@ -120,5 +131,21 @@ public class CardViewActivity extends Activity {
 
 	  }
 
-	
+		@Override
+		public boolean onOptionsItemSelected(MenuItem item) {
+			switch (item.getItemId()) {
+			case android.R.id.home:
+				// This ID represents the Home or Up button. In the case of this
+				// activity, the Up button is shown. Use NavUtils to allow users
+				// to navigate up one level in the application structure. For
+				// more details, see the Navigation pattern on Android Design:
+				//
+				// http://developer.android.com/design/patterns/navigation.html#up-vs-back
+				//
+				NavUtils.navigateUpFromSameTask(this);
+				return true;
+			}
+			return super.onOptionsItemSelected(item);
+		}
+
 }
--- CardManager/src/com/example/cardmanager/MainActivity.java
+++ CardManager/src/com/example/cardmanager/MainActivity.java
@@ -1,8 +1,15 @@
 package com.example.cardmanager;
 
 import android.os.Bundle;
+import android.app.ActionBar;
 import android.app.Activity;
+import android.content.Intent;
+import android.graphics.Typeface;
 import android.view.Menu;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class MainActivity extends Activity {
 
@@ -10,6 +17,53 @@ public class MainActivity extends Activity {
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
+		
+		//disable actionbar in the first activity
+		ActionBar actionBar = getActionBar();
+	    actionBar.hide();
+	    
+	    TextView app_name = (TextView) findViewById(R.id.appWelcomeLogo);
+	    Typeface cupcakeFontsStyle = CustomFontsLoader.getTypeface(getBaseContext(), CustomFontsLoader.FONT_NAME_3_CUPCAKES);
+	    app_name.setTypeface(cupcakeFontsStyle, Typeface.BOLD);
+	    	 
+	    TextView newsignUp = (TextView) findViewById(R.id.newsignUp);
+	    Typeface arialFontsStyle = CustomFontsLoader.getTypeface(getBaseContext(), CustomFontsLoader.FONT_NAME_1_ARIAL);
+	    newsignUp.setTypeface(arialFontsStyle);
+	    
+		//Sign In button
+		Button signInBtn = (Button) findViewById(R.id.signInButton);
+		signInBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {			
+            	startActivity(new Intent("com.example.cardmanager.CARDLIST"));		  
+			}
+		});
+		
+		//Sign In with google account
+		Button signInGoogleBtn = (Button) findViewById(R.id.signIn_google_Button);
+		signInGoogleBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {			
+            	Toast.makeText(getApplicationContext(), "Failed to sign in with Google account. Not implemented yet!", 
+            			Toast.LENGTH_SHORT).show();	  
+			}
+		});
+		
+		//Sign In with google account
+		Button signInFacebookBtn = (Button) findViewById(R.id.signIn_facebook_Button);
+		signInFacebookBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {			
+            	Toast.makeText(getApplicationContext(), "Failed to sign in with Facebook account. Not implemented yet!", 
+            			Toast.LENGTH_SHORT).show();	  
+			}
+		});
+		
+		//Sign Up 
+		Button signUpBtn = (Button) findViewById(R.id.signUpButton);
+		signUpBtn.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {			
+            	Toast.makeText(getApplicationContext(), "Under construction!", 
+            			Toast.LENGTH_SHORT).show();	  
+			}
+		});
 	}
 
 	@Override
